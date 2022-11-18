1c7ba2de670491735e5f211543258ac6127cada2
Kevin Choong <choong.kvn@gmail.com>
2022-11-18 17:07:23 +0100

    added basic navigation and sidebar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-(1..10).each do |i|
+(1..50).each do |i|
   User.create!(username: "testuser#{i}", email: "testuser#{i}@mail.azg", birthday: "1990-#{i}-#{i}")
 end
