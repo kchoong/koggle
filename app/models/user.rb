d94f86cdba35ae2ccc81a4c793a685f86fb0a0b0
Kevin Choong <choong.kvn@gmail.com>
2022-12-24 21:00:22 +0100

    fixed model with persons and organizations, added user for login using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,6 @@
 class User < ApplicationRecord
-
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A\w+@(\w+.)+\w+\Z/ }
-  validates :firstname, presence: false
-  validates :lastname, presence: false
-  validates :birthday, presence: false
-
-  belongs_to :group, optional: true
-
-  has_many :comments, as: :commentable
-
-  def name
-    "#{username}"
-  end
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
