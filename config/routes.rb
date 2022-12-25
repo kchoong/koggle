bf7cd105a2fd3303e4eb57751769f9349d53b9e5
Kevin Choong <choong.kvn@gmail.com>
2022-12-25 01:00:38 +0100

    basic login/register authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Root
   root "home#index"
 
+  # Authentication
+  devise_for :users
+
   # Model CRUD
   resources :persons do
     resources :comments, only: [:create, :destroy]
@@ -12,7 +15,6 @@ Rails.application.routes.draw do
   resources :organizations do
     resources :comments, only: [:create, :destroy]
   end
-  devise_for :users
 
   # GET
   get "/locale/:locale" => "home#switch_locale", as: "switch_locale"
